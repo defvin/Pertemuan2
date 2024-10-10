a9757bb9827edb73b2a0b34e616f6c4ca514d060
Tonni Ramdani M <49497725+tonni12345@users.noreply.github.com>
2024-10-10 08:58:42 +0700

    Update tugas1.php
    gini bang?

--- tugas1.php
+++ tugas1.php
@@ -1,1 +1,7 @@
-#buatakan kode variabel $nama setiap anggota menuliskan "hai saya nama-anda", dan nim anda
+<!-- buatakan kode variabel $nama setiap anggota menuliskan "hai saya nama-anda", dan nim anda -->
+<?php 
+    $nama = "Tonni Ramdani Mubaroq";
+    $nim = "10122051";
+
+    echo "hai saya ", $nama, "dan", "nim saya adalah ", $nim;
+?>
