7b3f4bfd5bae9e5430dbade3bbf5b6d8537697a7
ayuyuzh <123461027+ayuyuzh@users.noreply.github.com>
2024-10-10 08:59:40 +0700

    Update tugas1.php
    nama-nim

--- tugas1.php
+++ tugas1.php
@@ -1,1 +1,5 @@
-#buatakan kode variabel $nama setiap anggota menuliskan "hai saya nama-anda", dan nim anda
+#buatakan kode variabel $nama setiap anggota menuliskan "hai saya nama-anda", dan nim andaelse
+<?php
+ $nama = "triayu - 10122070";
+echo "NAMA-NIM". Nama;
+?>
