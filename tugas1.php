939afeb011ce17e566734a9a60929ef5829acb38
rekt085888 <rekt085888@gmail.com>
2024-10-10 09:00:33 +0700

    Update tugas1.php
    Doing the Task

--- tugas1.php
+++ tugas1.php
@@ -1,1 +1,6 @@
 #buatakan kode variabel $nama setiap anggota menuliskan "hai saya nama-anda", dan nim anda
+<?php
+$nama = "fadel";
+$nim = "10122049";
+echo "hai saya $nama, dan NIM saya $nim.";
+?>
